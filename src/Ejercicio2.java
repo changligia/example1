5e7ca3c5090e16c735fd2c85f1f1ec485cc56dd0
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-10 21:49:30 -0500

    Fin del Ejercicio

---
+++ src/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        var a = 1;
+        var b = 2;
+        var c = 3;
+        var d = 4;
+        var e = 5;
+
+        var x = String.valueOf(a) + String.valueOf(b) + String.valueOf(c) + String.valueOf(d) + String.valueOf(e);
+        System.out.println("El resultado es: " + x);
+    }
+}
