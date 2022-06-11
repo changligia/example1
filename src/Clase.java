e996eceb635b7aa1b27300b733e4d620dcad43ac
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-10 21:30:10 -0500

    Mi Tercer Commit

--- src/Clase.java
+++ src/Clase.java
@@ -2,5 +2,6 @@ public class Clase {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("hola mundo2");
+        System.out.println("hola mundo3");
     }
 }
