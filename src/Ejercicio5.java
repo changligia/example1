b2baea6c57e57dd1ab423a2b4392c42f9883a467
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-10 22:02:19 -0500

    Solucionando comentario del pull request

--- src/Ejercicio5.java
+++ src/Ejercicio5.java
@@ -2,8 +2,8 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         var a = 10;
         var b = 0;
-        var c = a <=0 && b == 10;
-        var d = a >=15 && b != 3;
+        var c = a <= 0 && b == 10;
+        var d = a >= 15 && b != 3;
 
         System.out.println("El valor de c es: " + c);
         System.out.println("El valor de d es: " + d);
